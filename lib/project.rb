afd76e3eec4b625140b0eb1b79b00c0700c593bd
Richard Melchior <raustin.melchior@gmail.com>
2018-06-06 03:31:56 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,14 @@
 class Project
-
-  attr_accessor :title, :backers
+  attr_accessor :backers, :title
 
   def initialize(title)
-    @title = title;
-    @backers = [];
+    @title = title
+    @backers = []
   end
 
-  def add_backer(backer)
-    @backers << backer;
-    backer.backed_projects << self;
-  end
+def add_backer(backer)
+  @backers << backer
+  backer.backed_projects = self
+end
+
 end
